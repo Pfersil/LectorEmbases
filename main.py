1880f15ad2f28c7a0448cbe0a45ac1162d717925
Pablo <pafernandez.silva@gmail.com>
2021-06-25 10:05:44 +0200

    V2
    The algorithm still looking for a rectangle but  now it reads all rectangles and only display data if an structure like this "xx/xx/xx" is found

--- main.py
+++ main.py
@@ -25,33 +25,56 @@ def getContours(img, imgContour):
     global imgCropped
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # Find contours
 
-    imgDrawCount = imgContour.copy()  # Just an image to draw
     for cnt in contours:
 
-        area = cv2.contourArea(cnt)  # Calculate area for every contour
         peri = 0.02 * cv2.arcLength(cnt, True)  # Calculate perimeter for every contour
         approx = cv2.approxPolyDP(cnt, peri, True)
 
-        if 100000 > area > 45000 and len(approx) == 4:  # Search for rectangles with a concrete area
+        if len(approx) == 4:  # Search for rectangles with a concrete area
             # Color detection would be nice to have
 
             x, y, w, h = cv2.boundingRect(approx)
 
-            # Code to draw a bounding rectangle and its area
-            # Its disabled because its affects tesseract reads
-
-            # cv2.rectangle(imgDrawCount, (x, y), (x + w, y + h), (255, 0, 255), 7)
-            # cv2.putText(imgDrawCount, str(area), (x, y - 5), 1, 1.5, (0, 255, 0), 2)
-
-            # Code to draw a tilted bounding rectangle.
-            # Its disabled because its affects tesseract reads
-
-            # rect = cv2.minAreaRect(cnt)
-            # box = cv2.boxPoints(rect)
-            # box = np.int0(box)
-            # cv2.drawContours(imgContour, [box], 0, (0, 0, 255), 2)
-
             imgCropped = imgContour[y:y + h, x:x + w]  # Crop image using bounding rect coordinates
+
+            # Read text form cropped image
+            imgRead = cv2.cvtColor(imgCropped, cv2.COLOR_BGR2RGB)  # tesseract works with RGB
+            kernel2 = np.ones((2, 2), np.uint8)
+            imgRead = cv2.dilate(imgRead, kernel2, iterations=1)
+            imgRead = cv2.erode(imgRead, kernel2, iterations=1)
+            string = pytesseract.image_to_string(imgRead)  # Read and put text in a string
+
+
+            # Using Naive Method
+            res = None
+            for i in range(0, len(string)):    # Search a xx\xx\xx structure in the string
+                if string[i] == "/" and string[i-3] == "/":
+                    res = i + 1
+                    break
+
+            if res is None:                          # If not found, pass
+                pass
+            else:                                    # If found, extract the data from the string
+                data = string[res-5:res+2]
+                print(data)
+
+            res1 = None
+            for i in range(0, len(string)):    # Search a xx\xx\xx structure in the string
+                if string[i] == "L":                 # If not found, pass
+                    res1 = i + 1
+                    break
+
+            if res1 is None:                         # If found, extract the data from the string
+                pass
+            else:
+                data1 = string[res1-1:res1 + 7]
+                print(data1)
+
+            with open('Data.txt', 'w') as f:  # Create/ open 'Data.txt'
+                f.write(string)  # Wirte string in txt
+                f.write('\n')
+
+            #print(string)
 
     return imgCropped
 
@@ -78,29 +101,21 @@ while True:
     kernel = np.ones((5, 5))
     imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
-    # Call getContours function and store it in 'imgCroped'
+    # Call getContours function (read image) and store it in 'imgCroped'
     imgCropped = getContours(imgDil, imgContour)
 
     # Just some stuff to scale down the image
-    scale_percent = 50  # percent of original size
+
+    """scale_percent = 50  # percent of original size
     width = int(imgContour.shape[1] * scale_percent / 100)
     height = int(imgContour.shape[0] * scale_percent / 100)
     dim = (width, height)
 
     resized = cv2.resize(imgContour, dim, interpolation=cv2.INTER_AREA)
 
-    # Show images
-    cv2.imshow("Result", resized)
-    cv2.imshow("Result2", imgCropped)
-
-    # Read text form cropped image
-    imgRead = cv2.cvtColor(imgCropped, cv2.COLOR_BGR2RGB)  # tesseract works with RGB
-    string = pytesseract.image_to_string(imgRead)  # Read and put text in a string
-    with open('Data.txt', 'w') as f:  # Create/ open 'Data.txt'
-        f.write(string)  # Wirte string in txt
-        f.write('\n')
-
-    print(string)
+     Show images
+     cv2.imshow("Result", resized)
+     cv2.imshow("Result2", imgCropped)"""
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
