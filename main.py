fe1dda546224465e647b976e65fbbaf10214cfc6
Pablo <pafernandez.silva@gmail.com>
2021-06-26 16:26:28 +0200

    V3

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import pytesseract
 import numpy as np
 
 
-def empty(a):
+# import time
+
+def empty():
     pass
 
+
+digits = "0123456789"
 
 # Tesseract is used to read text in images
 # Must be instaled in the computer
@@ -13,17 +17,12 @@ def empty(a):
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-# A screen in order to change on air some parameters
-cv2.namedWindow("Parameters")
-cv2.resizeWindow("Parameters", 640, 240)
-cv2.createTrackbar("Threshold1", "Parameters", 73, 255, empty)
-cv2.createTrackbar("Threshold2", "Parameters", 34, 255, empty)
-
 
 # Get Contours and detect squares
-def getContours(img, imgContour):
-    global imgCropped
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # Find contours
+def read_img(img_in, imgContour_in):
+    data0 = "None"
+    data1 = "None"
+    contours, hierarchy = cv2.findContours(img_in, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)  # Find contours
 
     for cnt in contours:
 
@@ -32,51 +31,52 @@ def getContours(img, imgContour):
 
         if len(approx) == 4:  # Search for rectangles with a concrete area
             # Color detection would be nice to have
-
             x, y, w, h = cv2.boundingRect(approx)
 
-            imgCropped = imgContour[y:y + h, x:x + w]  # Crop image using bounding rect coordinates
+            imgCropped = imgContour_in[y:y + h, x:x + w]  # Crop image using bounding rect coordinates
 
             # Read text form cropped image
-            imgRead = cv2.cvtColor(imgCropped, cv2.COLOR_BGR2RGB)  # tesseract works with RGB
-            kernel2 = np.ones((2, 2), np.uint8)
-            imgRead = cv2.dilate(imgRead, kernel2, iterations=1)
-            imgRead = cv2.erode(imgRead, kernel2, iterations=1)
-            string = pytesseract.image_to_string(imgRead)  # Read and put text in a string
+            imgCropped = cv2.cvtColor(imgCropped, cv2.COLOR_BGR2RGB)  # tesseract works with RGB
 
-
+            string = pytesseract.image_to_string(imgCropped)  # Read and put text in a string
+            # print(string)
             # Using Naive Method
             res = None
-            for i in range(0, len(string)):    # Search a xx\xx\xx structure in the string
-                if string[i] == "/" and string[i-3] == "/":
+            for i in range(0, len(string)):  # Search a xx\xx\xx structure in the string
+                if string[i] == "/" and string[i - 3] == "/":
                     res = i + 1
                     break
 
-            if res is None:                          # If not found, pass
+            if res is None:  # If not found, pass
                 pass
-            else:                                    # If found, extract the data from the string
-                data = string[res-5:res+2]
-                print(data)
+            else:  # If found, extract the data from the string
+                data0 = string[res - 6:res + 2]
+                # print(data0)
 
             res1 = None
-            for i in range(0, len(string)):    # Search a xx\xx\xx structure in the string
-                if string[i] == "L":                 # If not found, pass
-                    res1 = i + 1
-                    break
+            count = 0
+            length = 6
+            for i in range(0, len(string)):  # Search a xx\xx\xx structure in the string
+                if string[i] == "L":  # If not found, pass
+                    substring = string[i:i + length]
+                    for j in range(0, len(digits)):
+                        if digits[j] in substring:
+                            count += 1
+                            # print(count)
+                        else:
+                            # print("else")
+                            pass
+                        if count >= length - 2:
+                            res1 = i + 1
+                            break
 
-            if res1 is None:                         # If found, extract the data from the string
+            if res1 is None:  # If found, extract the data from the string
                 pass
             else:
-                data1 = string[res1-1:res1 + 7]
-                print(data1)
+                data1 = string[res1 - 1:res1 + length]
+                # print(data1)
 
-            with open('Data.txt', 'w') as f:  # Create/ open 'Data.txt'
-                f.write(string)  # Wirte string in txt
-                f.write('\n')
-
-            #print(string)
-
-    return imgCropped
+    return data0, data1
 
 
 while True:
@@ -87,26 +87,33 @@ while True:
     # Next lines are just for preprocessing the image
 
     imgContour = img.copy()
-    cropped = img.copy()
 
     imgBlur = cv2.GaussianBlur(img, (5, 5), 1)
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
 
-    # Get values from sliders
-    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
-    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
-
     # Continue with preprocessing
-    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+    imgCanny = cv2.Canny(imgGray, 73, 34)
     kernel = np.ones((5, 5))
     imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
     # Call getContours function (read image) and store it in 'imgCroped'
-    imgCropped = getContours(imgDil, imgContour)
+    data = read_img(imgDil, imgContour)
+
+    date = data[0]
+    lote = data[1]
+
+    print(date)
+    print(lote)
+
+    with open('Data.txt', 'w') as f:  # Create/ open 'Data.txt'
+        f.write(date)  # Write string in txt
+        f.write('\n')
+        f.write(lote)
+        f.write('\n')
 
     # Just some stuff to scale down the image
 
-    """scale_percent = 50  # percent of original size
+    """ scale_percent = 50  # percent of original size
     width = int(imgContour.shape[1] * scale_percent / 100)
     height = int(imgContour.shape[0] * scale_percent / 100)
     dim = (width, height)
@@ -115,7 +122,7 @@ while True:
 
      Show images
      cv2.imshow("Result", resized)
-     cv2.imshow("Result2", imgCropped)"""
+     cv2.imshow("Result2", imgCropped) """
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
